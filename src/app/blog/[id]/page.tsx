adcdd0e015b463015c0fe2da31d946d9267d71a4
radiyakhan <168372745+radiyakhan@users.noreply.github.com>
2025-01-02 20:28:08 +0500

    Update page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -35,12 +35,7 @@ interface Paramst {
                {find?.description2}
               </p>
               <div className="flex justify-center">
-                <button className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
-                  Button
-                </button>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                  Button
-                </button>
+                
               </div>
             </div>
           </div>
